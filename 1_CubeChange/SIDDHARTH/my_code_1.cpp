f0aaeb5ca5effa73f7e3ec9e301b75fa1b120f7a
SIDDHARTH <shashikarsidharth@gmail.com>
2016-02-01 19:28:31 -0800

    final commit

--- 1_CubeChange/SIDDHARTH/my_code_1.cpp
+++ 1_CubeChange/SIDDHARTH/my_code_1.cpp
@@ -2,37 +2,21 @@
 
 using namespace std;
 
-int volume(int N)
-{
-    return N*N*N;
-}
-
 int main()
 {
-   long unsigned int  T;
-   long unsigned int  N;
-    //cout << "enter the number of test cases" <<endl;
+    int  T;
+    long long  N;
     cin  >> T;
-    if(T==0)
-        {
-    //        cout << "sorry  wrong input"<<endl;
-            return 1;
-        }
-
     while(T!=0)
     {
-        //cout << "enter the value of the sides of the cube" <<endl;
         cin >> N;
         if(N==1)
-        {
-        cout <<volume(N)<<endl;
-        }
+        {cout<<1<<endl;}
         else
-        {
-        cout <<volume(N)-1<<endl;
-        }
+        {cout <<N*N*N-(N-2)*(N-2)*(N-2)<<endl;}
         T--;
     }
 
     return 0;
 }
+
